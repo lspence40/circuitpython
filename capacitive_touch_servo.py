2cbca597d413504adf4a14438e66bae7ee35e926
lspence40 <55842469+lspence40@users.noreply.github.com>
2020-01-02 12:05:30 -0500

    added comments

--- capacitive_touch_servo.py
+++ capacitive_touch_servo.py
@@ -1,26 +1,29 @@
 import time
 import board
-import pulseio
+import pulseio #libraries
 from adafruit_motor import servo
 import touchio
 
 pwm = pulseio.PWMOut(board.A2, duty_cycle = 2 ** 15, frequency = 50)
-my_servo = servo.Servo(pwm)
+my_servo = servo.Servo(pwm) #prepare servo
 
 touch_A0 = touchio.TouchIn(board.A0)
-touch_A5 = touchio.TouchIn(board.A5)
+touch_A5 = touchio.TouchIn(board.A5) #prepare touchable wire thingies
 
-angle = 90
+angle = 90 #variables
 speed = 0.2
 
-while True:
+while True: #repeat forever
 
     if touch_A0.value:
-        angle += speed
+        angle += speed #go one way
+        
     if touch_A5.value:
-        angle -= speed
+        angle -= speed #go the other way
+        
     if angle > 180:
         angle = 180
-    if angle < 0:
+    if angle < 0: #constrain angle
         angle = 0
-    my_servo.angle = angle
+        
+    my_servo.angle = angle #move the servo to the designated position
