f36af187d6edf6972a28e3998d59d7c153c63696
lspence40 <55842469+lspence40@users.noreply.github.com>
2020-01-05 10:51:55 -0500

    added comments

--- fading_led.py
+++ fading_led.py
@@ -1,22 +1,22 @@
-import board
+import board #libraries
 from analogio import AnalogOut
 
-analog_out = AnalogOut(board.A0)
+analog_out = AnalogOut(board.A0) #setup LED
 
-abc = 1
+abc = 1 #variables
 power = 0
 speed = 3
 
-while True:
+while True: #repeat
 
     if(abc):
-        power += speed
+        power += speed #power goes up
     else:
-        power -= speed
+        power -= speed #and also down
 
     if(power >= 65000):
         abc = 0
-    if(power <= 0):
+    if(power <= 0): #this is the thing that decides whether power goes up or down
         abc = 1
 
-    analog_out.value = power
+    analog_out.value = power #run power through the LED
