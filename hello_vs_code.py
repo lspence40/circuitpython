3d787552791bf1f709ebc1cc5ccd72c300963230
lspence40 <55842469+lspence40@users.noreply.github.com>
2020-01-05 10:27:46 -0500

    added comments

--- hello_vs_code.py
+++ hello_vs_code.py
@@ -1,8 +1,8 @@
-import time
+import time #library
 
 message = "hi"
-speed = .5
+speed = .5 #variables
 
 while True:
     print(str(message))
-    time.sleep(speed)
+    time.sleep(speed) #say 'hi' twice per second for the rest of eternity
