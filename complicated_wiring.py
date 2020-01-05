f949aabfa8658df78bde538bafbaa66b763e5aec
lspence40 <55842469+lspence40@users.noreply.github.com>
2020-01-05 10:54:35 -0500

    added comments

--- complicated_wiring.py
+++ complicated_wiring.py
@@ -1,10 +1,10 @@
-from fancyLED import FancyLED
+from fancyLED import FancyLED #imports my custom library
 
 fancy1 = FancyLED(2,3,4)
-fancy2 = FancyLED(5,6,7)
+fancy2 = FancyLED(5,6,7) #sets up LEDs
 
 while True:
     fancy1.alternate()
-    fancy2.blink()
+    fancy2.blink() #uses the library
     fancy1.chase()
     fancy2.sparkle()
